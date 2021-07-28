5e11b74789c4629dd8e5d0eaf1acf7609b6ac830
christear <zb.picb@gmail.com>
2021-07-28 17:05:55 +0300

    fixed bug to extract intron
    fix the bug in the bash script to extract intron

--- get_intron_from_STAR_output.sh
+++ get_intron_from_STAR_output.sh
@@ -33,7 +33,7 @@ do
         }else{
             print $1,"JUNC","INTRON",$2 - OVER_HANG + 1,$2 + OVER_HANG,".","-",".","intron_id "$1":"$2"-"$3" ; ss_type "$5" ; anno_type "$6" ; sout_count "$7
         }
-    }' | grep "^[1-9XY]" >  $out_dir/$SAMPLEID.intron.left.gtf
+    }' | grep "^[1-9XY]" >  $out_dir/$SAMPLEID.intron.right.gtf
     ###
     cat $data_path/$SAMPLEID.SJ.out.tab | awk -v OVER_HANG=$OVER_HANG '$2 > OVER_HANG' | awk -v OVER_HANG=$OVER_HANG '{OFS="\t"} {
         if($4 == 1){
